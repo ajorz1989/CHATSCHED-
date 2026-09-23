f21d2c59298bec133f84a250d79f71024eef8eba
ajorz1989 <ajoranzie@gmail.com>
2026-09-23 18:39:17 +0200

    Fix Super Tools internal navigation

--- src/components/AdminNavigation.tsx
+++ src/components/AdminNavigation.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import {
   BarChart3,
   BriefcaseBusiness,
@@ -199,13 +200,13 @@ export default function AdminNavigation({
 
                       if (item.href) {
                         return (
-                          <a
+                          <Link
                             key={item.label}
-                            href={item.href}
+                            to={item.href}
                             className="flex items-center gap-2.5 rounded-lg px-2.5 py-2 text-xs text-white/65 hover:text-white hover:bg-white/[0.05] transition"
                           >
                             {content}
-                          </a>
+                          </Link>
                         );
                       }
 
